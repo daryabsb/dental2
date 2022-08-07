60ff44237bccd323f34441869d835b436e0b5c30
unknown <daryabsb@gmail.com>
2022-08-08 01:25:51 +0300

    Some directives and nav in mainnav

--- spa/cleanup/playground.js
+++ spa/cleanup/playground.js
@@ -14,12 +14,12 @@
 // console.log(developer.salary);
 // console.log(developer.lookingForWork);
 
-export const evenOrOdd = (num) => {
-    if (num % 2 === 0) {
-        return "Even";
-    } else {
-        return "Odd";
-    }
-};
+// export const evenOrOdd = (num) => {
+//     if (num % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// };
 
-export const multiply = (num1, num2) => num1 * num2;
+// export const multiply = (num1, num2) => num1 * num2;
--- spa/src/components/MainNav.vue
+++ spa/src/components/MainNav.vue
@@ -1,34 +1,51 @@
 <template>
-  <header class="w-full text-sm">Main Nav</header>
-  <div class="fixed top-0 left-0 w-full h-16 bg-white">
-    <div
-      class="
-        flex flex-nowrap
-        h-full
-        px-8
-        mx-auto
-        border-b border-solid border-brand-gray-1
-      "
-    >
-      <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
-      <h2 class="flex items-center h-full ml-8">
-        Developed by: {{ author.firstName + " " + author.lastName }}
-      </h2>
-    </div>
-  </div>
+	<header class="w-full text-sm">Main Nav</header>
+	<div class="fixed top-0 left-0 w-full h-16 bg-white">
+		<div class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1">
+			<a :href="url" class="flex items-center h-full text-xl">{{ company }}</a>
+			<nav class="h-full ml-12">
+				<ul class="flex h-full p-0 m0 list-none">
+					<li class="h-ful ml-9 first:ml-0" v-for="item in menuItems" :key="item.id">
+						<a :href="item.url" class="flex items-center h-full py-2 5">{{item.title}}</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "MainNav",
-  data() {
-    return {
-      company: "ARON",
-      author: {
-        firstName: "Darya",
-        lastName: "Ibrahim",
-      },
-    };
-  },
+	name: "MainNav",
+	data() {
+		return {
+			company: "ARON",
+			url: "https://careers.google.com",
+			menuItems: [
+        {
+					id: 1,
+					title: "Teams",
+					url: "https://careers.google.com",
+				},
+				{
+					id: 2,
+					title: "Locations",
+					url: "https://careers.google.com",
+				},
+				{
+					id: 3,
+					title: "Life at ARON",
+					url: "https://careers.google.com",
+				},
+				{
+					id: 4,
+					title: "How we hire",
+					url: "https://careers.google.com",
+				},
+				{ id: 5, title: "Students", url: "https://careers.google.com" },
+				{ id: 6, title: "Jobs", url: "https://careers.google.com" },
+			],
+		};
+	},
 };
 </script>
