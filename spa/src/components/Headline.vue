ccc4bdbf701f59e9a463bbb16596e5cb4e5b1ca8
unknown <daryabsb@gmail.com>
2022-08-10 07:50:52 +0300

    Created headline com

--- spa/src/components/Headline.vue
+++ spa/src/components/Headline.vue
@@ -1,20 +1,67 @@
 <template>
-  <section>
-    <h1>Build for everyone</h1>
-    <h2>Find your next job @Aron</h2>
-  </section>
+	<section>
+		<h1>
+			<span :class="actionClasses">{{action }}</span>
+			<br />for everyone
+		</h1>
+		<h2>Find your next job @Aron</h2>
+	</section>
 </template>
 
 <script>
 export default {
-  name: "Headline",
-  data() {
-    return {
-      sample: "Hello",
-    };
-  },
-  created() {
-    console.log("I've been created! Look: ", this.sample);
-  },
+	name: "Headline",
+	data() {
+		return {
+			action: "Build",
+
+			interval: null,
+		};
+	},
+	computed: {
+		actionClasses() {
+			return {
+				build: this.action === "build",
+				create: this.action === "Create",
+				design: this.action === "Design",
+				code: this.action === "Code",
+			};
+		},
+	},
+	created() {
+		this.changeTitle();
+	},
+	beforeUnmount() {
+		clearInterval(this.interval);
+	},
+	methods: {
+		changeTitle() {
+			setInterval(() => {
+				const actions = ["Build", "Create", "Design", "Code"];
+				const currentActionIndex = actions.indexOf(this.action);
+				const nextActionIndex = (currentActionIndex + 1) % 4;
+				const nextAction = actions[nextActionIndex];
+				this.action = nextAction;
+			}, 2800);
+		},
+	},
 };
 </script>
+
+<style scoped>
+.build {
+	color: #1a73e8;
+}
+
+.create {
+	color: #34a853;
+}
+
+.design {
+	color: #f9ab00;
+}
+
+.code {
+	color: #d93025;
+}
+</style>
