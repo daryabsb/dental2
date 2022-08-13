0ad384d14206ac9d60d2764be06a6fba19bbde3e
Darya <daryabsb@gmail.com>
2022-08-13 17:10:49 +0300

    Fixed hero comp

--- spa/src/views/HomeView.vue
+++ spa/src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
-  <job-search-form></job-search-form>
+  <hero></hero>
 </template>
 
 <script>
 // @ is an alias to /src
+import Hero from "@/components/JobSearch/Hero.vue";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
 
 export default {
   name: "HomeView",
   components: {
+    Hero,
     JobSearchForm,
   },
 };
