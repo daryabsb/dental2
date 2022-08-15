ce4609a220e69b8599e9a1c185b370984c763111
unknown <daryabsb@gmail.com>
2022-08-15 23:48:43 +0300

    Axios done

--- spa/src/components/JobResults/JobListing.vue
+++ spa/src/components/JobResults/JobListing.vue
@@ -1,63 +1,53 @@
 <template>
-  <li class="mb-7">
-    <router-link
-      to="/jobs/results/1"
-      class="
-        block
-        mx-auto
-        bg-white
-        border border-solid border-brand-gray-2
-        rounded
-        hover:shadow-gray
-      "
-    >
-      <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
-        <h2 class="mb-2 text-2xl">
-          {{ job.title }}
-        </h2>
-        <div class="flex flex-ro align-middle">
-          <div class="mr-5">
-            <span>{{ job.organization }}</span>
-          </div>
-          <div class="mr-5">
-            <span v-for="loc in job.locations" :key="loc">{{ loc }} -</span>
-          </div>
-        </div>
-      </div>
-      <div class="px-8 py-4">
-        <div class="">
-          <h3 class="mt-1 mb2">Qualifications:</h3>
-          <div>
-            <ul class="pl-8 list-disc">
-              <li v-for="qual in job.minimumQualifications" :key="qual">
-                {{ qual }}<br />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="mt-2 text-center">
-          <router-link :to="jobPageLink" class="text-brand-blue-1"
-            >Expand</router-link
-          >
-        </div>
-      </div>
-    </router-link>
-  </li>
+	<li class="mb-7">
+		<router-link
+			:to="jobPageLink"
+			class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
+		>
+			<div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
+				<h2 class="mb-2 text-2xl">{{ job.title }}</h2>
+				<div class="flex flex-ro align-middle">
+					<div class="mr-5">
+						<span>{{ job.organization }}</span>
+					</div>
+					<ul>
+						<li v-for="loc in job.locations" :key="loc" class="inline-block mr-5">{{ loc }}</li>
+					</ul>
+				</div>
+			</div>
+			<div class="px-8 py-4">
+				<div class>
+					<h3 class="mt-1 mb2">Qualifications:</h3>
+					<div>
+						<ul class="pl-8 list-disc">
+							<li v-for="qual in job.minimumQualifications" :key="qual">
+								{{ qual }}
+								<br />
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="mt-2 text-center">
+					<router-link :to="jobPageLink" class="text-brand-blue-1">Expand</router-link>
+				</div>
+			</div>
+		</router-link>
+	</li>
 </template>
 
 <script>
-export default {
-  name: "JobListing",
-  props: {
-    job: {
-      type: Object,
-      required: true,
-    },
-  },
-  computed: {
-    jobPageLink() {
-      return `/jobs/results/${this.job.id}`;
-    },
-  },
-};
+	export default {
+		name: "JobListing",
+		props: {
+			job: {
+				type: Object,
+				required: true,
+			},
+		},
+		computed: {
+			jobPageLink() {
+				return `/jobs/results/${this.job.id}`;
+			},
+		},
+	};
 </script>
--- spa/src/components/navs/MainNav.vue
+++ spa/src/components/navs/MainNav.vue
@@ -1,106 +1,87 @@
 <template>
-  <header :class="['w-full', 'text-sm', headerHeightClass]">Main Nav</header>
-  <div class="fixed top-0 left-0 w-full h-16 bg-white">
-    <div
-      class="
-        flex flex-nowrap
-        h-full
-        px-8
-        mx-auto
-        border-b border-solid border-brand-gray-1
-      "
-    >
-      <router-link to="/" class="flex items-center h-full text-xl">{{
-        company
-      }}</router-link>
-      <nav class="h-full ml-12">
-        <ul class="flex h-full p-0 m0 list-none">
-          <li
-            v-for="item in menuItems"
-            :key="item.id"
-            class="h-ful ml-9 first:ml-0"
-            data-test="main-nav-list-item"
-          >
-            <router-link :to="item.url" class="flex items-center h-full py-2 5">
-              {{ item.title }}
-            </router-link>
-          </li>
-        </ul>
-      </nav>
-      <div class="flex items-center h-full ml-auto">
-        <ProfileImage
-          v-if="isLoggedIn"
-          data-test="profile-image"
-          @click="isLoggedIn = false"
-        />
-        <action-button
-          v-else
-          type="primary"
-          text="Sign in"
-          data-test="login-button"
-          @click="loginUser"
-        ></action-button>
-      </div>
-    </div>
-    <sub-nav v-if="isLoggedIn" data-test="sub-nav"></sub-nav>
-  </div>
+	<header :class="['w-full', 'text-sm', headerHeightClass]">Main Nav</header>
+	<div class="fixed top-0 left-0 w-full h-16 bg-white">
+		<div class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1">
+			<router-link to="/" class="flex items-center h-full text-xl">
+				{{
+				company
+				}}
+			</router-link>
+			<nav class="h-full ml-12">
+				<ul class="flex h-full p-0 m0 list-none">
+					<li
+						v-for="item in menuItems"
+						:key="item.id"
+						class="h-ful ml-9 first:ml-0"
+						data-test="main-nav-list-item"
+					>
+						<router-link :to="item.url" class="flex items-center h-full py-2 5">{{ item.title }}</router-link>
+					</li>
+				</ul>
+			</nav>
+			<div class="flex items-center h-full ml-auto">
+				<ProfileImage v-if="isLoggedIn" data-test="profile-image" @click="isLoggedIn = false" />
+				<action-button v-else type="primary" text="Sign in" data-test="login-button" @click="loginUser"></action-button>
+			</div>
+		</div>
+		<sub-nav v-if="isLoggedIn" data-test="sub-nav"></sub-nav>
+	</div>
 </template>
 
 <script>
-import ActionButton from "@/components/shared/ActionButton.vue";
-import ProfileImage from "@/components/navs/ProfileImage.vue";
-import SubNav from "@/components/navs/SubNav.vue";
+	import ActionButton from "@/components/shared/ActionButton.vue";
+	import ProfileImage from "@/components/navs/ProfileImage.vue";
+	import SubNav from "@/components/navs/SubNav.vue";
 
-export default {
-  name: "MainNav",
-  components: {
-    ActionButton,
-    ProfileImage,
-    SubNav,
-  },
-  data() {
-    return {
-      company: "ARON",
-      isLoggedIn: false,
-      menuItems: [
-        {
-          id: 1,
-          title: "Teams",
-          url: "/",
-        },
-        {
-          id: 2,
-          title: "Locations",
-          url: "/",
-        },
-        {
-          id: 3,
-          title: "Life at ARON",
-          url: "/",
-        },
-        {
-          id: 4,
-          title: "How we hire",
-          url: "/",
-        },
-        { id: 5, title: "Students", url: "/" },
-        { id: 6, title: "My Test", url: "/mytest" },
-        { id: 7, title: "Jobs", url: "/jobs/results" },
-      ],
-    };
-  },
-  computed: {
-    headerHeightClass() {
-      return {
-        "h-16": !this.isLoggedIn,
-        "h-32": this.isLoggedIn,
-      };
-    },
-  },
-  methods: {
-    loginUser() {
-      this.isLoggedIn = true;
-    },
-  },
-};
+	export default {
+		name: "MainNav",
+		components: {
+			ActionButton,
+			ProfileImage,
+			SubNav,
+		},
+		data() {
+			return {
+				company: "ARON",
+				isLoggedIn: false,
+				menuItems: [
+					{
+						id: 1,
+						title: "Teams",
+						url: "/",
+					},
+					{
+						id: 2,
+						title: "Locations",
+						url: "/",
+					},
+					{
+						id: 3,
+						title: "Life at ARON",
+						url: "/",
+					},
+					{
+						id: 4,
+						title: "How we hire",
+						url: "/",
+					},
+					{ id: 5, title: "Students", url: "/" },
+					{ id: 6, title: "Jobs", url: "/jobs/results" },
+				],
+			};
+		},
+		computed: {
+			headerHeightClass() {
+				return {
+					"h-16": !this.isLoggedIn,
+					"h-32": this.isLoggedIn,
+				};
+			},
+		},
+		methods: {
+			loginUser() {
+				this.isLoggedIn = true;
+			},
+		},
+	};
 </script>
