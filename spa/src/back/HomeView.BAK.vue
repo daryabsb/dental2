41c4887924d4de65aa1354149c63e6bcc4e01bff
Darya <daryabsb@gmail.com>
2022-08-31 12:54:07 +0300

    Migrating to typescript

---
+++ spa/src/back/HomeView.BAK.vue
@@ -0,0 +1,23 @@
+<template>
+	<hero></hero>
+	<spotlight></spotlight>
+</template>
+
+<script>
+	// @ is an alias to /src
+	import Hero from "@/components/JobSearch/Hero.vue";
+	import Spotlight from "@/components/JobSearch/Spotlight.vue";
+
+	export default {
+		name: "HomeView",
+		components: {
+			Hero,
+			Spotlight,
+		},
+		methods: {
+			fetchJobs() {
+				this.$store.dispatch("FETCH_JOBS");
+			},
+		},
+	};
+</script>
